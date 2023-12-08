d8ab2c88352720591d5d248641d7b2a8e2e1d76d
wwCharles <charleswaweruw@gmail.com>
2023-12-08 18:25:25 +0300

    welcom modal change

--- client/src/components/WelcomeModal.jsx
+++ client/src/components/WelcomeModal.jsx
@@ -5,17 +5,20 @@ const WelcomeModal = ({ onClose }) => {
   return (
     <div className="modal-overlay ">
       <div className="modal-content border">
+        <h1 className="h3-bold md:h2-bold">juiced23</h1>
         <p>
-          Juiced23: A hub for everything automotive, showcase builds & share
-          stories.
+          {/* Juiced23
+          <br /> */}
+          A hub for everything automotive, showcase builds & share stories.
           <br />
           Community-driven – flag any inappropriate post.
           <br />
           Reach out for functionality requests, bugs, or to join our team at
           <br />
           <a href={`mailto:${email}`}>{email}</a>
-          <br />
-          Your data? We don't care. Ads keep us rolling – be cool.
+          {/* <br />
+          Your data? We don't care. 
+          Ads keep us rolling – be cool. */}
           {/* <br />
            *Don't make this site ig/tiktok, keep it real* */}
         </p>
