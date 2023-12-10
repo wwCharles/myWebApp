003f3d3f37cd1de3b0b8a68a5a838070ce1dbad6
wwCharles <charleswaweruw@gmail.com>
2023-12-10 16:53:36 +0300

    monetag

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import PostForm from "../components/PostForm";
+import AdsComponent from "../components/AdsComponent";
 
 export default function CreatePost() {
   return (
     <div className="flex flex-1 ">
+      {/* <AdsComponent dataAdSlot="6097104098" /> */}
       <div className="common-container ">
         <div className="max-w-5xl flex-start gap-3 justify-start w-full">
           <h2 className="h3-bold md:h2-bold text-left w-full">Create Post</h2>
